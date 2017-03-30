29102596afd597d125b63129aa18eb0c78097b3e
Ronen <RonenNess@gmail.com>
2017-03-30 22:28:58 +0200

    ..

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
   packages = ['grepfunc'],
   package_data = {'grepfunc' : ["*.py", "README.md"], },
   version = '1.0.0',
-  description = 'Unix\'s grep, as a Python function.',
+  description = 'Unix\'s grep, implemented as a Python function.',
   author = 'Ronen Ness',
   author_email = 'ronenness@gmail.com',
   url = 'https://github.com/RonenNess/grepfunc',
   download_url = 'https://github.com/RonenNess/grepfunc/tarball/1.0.0',
-  keywords = ['grep', 'regex', 'filter', 'search'],
+  keywords = ['grep', 'regex', 'filter', 'search', 'strings'],
   classifiers = [],
 )
